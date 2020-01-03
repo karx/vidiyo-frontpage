483767f0a97fe79eb790524d05e171429a23f02c
kaaro <kartik.arora1214@gmail.com>
2020-01-04 00:08:07 +0530

    dev update

--- js/marker-specific-show.js
+++ js/marker-specific-show.js
@@ -1,39 +1,31 @@
 
 AFRAME.registerComponent('marker-specific-parent', {
     schema: {
-        markers: { type: 'string', default: '' },
-        pos_transitions: { type: 'string', default: '' },
+        marker: { type: 'string', default: '' },
+        current_marker: { type: 'string', default: '_' },
         rot_transitions: { type: 'string', default: '' }
     },
     multiple: true,
 
     init: function () {
-        var sceneEl = document.querySelector('a-scene');
-        sceneEl.setAttribute('background', 'color: #04060F');
-        sceneEl.setAttribute('vr-mode-ui', 'enabled: false');
-
-        var stringToLog = this.data.markers;
-        //   this.data.initPosZ = -10;
-        this.data.initPosX = 0;
-        this.data.last_state = 0;
-        this.last_state_index = -1;
-        console.log(stringToLog);
-        console.log('hEader Phone');
+        console.log(`Event Specific for ${this.data.marker} `);
     },
 
     update: function () {
         var data = this.data;  // Component property values.
         var el = this.el;  // Reference to the component's entity.
-
-        if (data.event) {
+        console.log(`Child data: ${data}`);
+        console.log(`Current Marker Data from DOM: ${this.el.getAttribute('current_marker')}`);
+        
+        if (data.marker == data.current_marker) {
             // This will log the `message` when the entity emits the `event`.
-            el.addEventListener(data.event, function () {
-                console.log(data.message);
-            });
+            el.setAttribute("visible",true);
+            console.log(`showing entity`);
         } else {
 
             // `event` not specified, just log the message.
-            console.log(data.message);
+            el.setAttribute("visible",true);
+            console.log(`hiding entity`);
         }
     },
 
@@ -53,7 +45,7 @@ AFRAME.registerComponent('marker-specific-parent', {
         let markers_distances = markers_ids.map((x) => { return (Math.abs(calcMidFromOffsets(x) - t_vh_mid)) });
 
         //DEBUG LINE
-        // document.getElementById('center-marker').style.top = t_vh_mid + 'px';
+        document.getElementById('center-marker').style.top = t_vh_mid + 'px';
 
         let minState = Math.min(...markers_distances);
 
@@ -62,27 +54,17 @@ AFRAME.registerComponent('marker-specific-parent', {
         for (let i = 0; i < markers_distances.length; i++) {
             let eachMarkerDistance = markers_distances[i];
             if (Math.abs(minState - eachMarkerDistance) < 0.001) {
-                
                 posOfMarker = data.pos_transitions.split('|')[i].trim().split(',');
                 pos = tinyMoveTo(posOfMarker[0], posOfMarker[1], posOfMarker[2], pos, 0.3);
 
                 rotOfMarker = data.rot_transitions.split('|')[i].trim().split(',');
                 rot = tinyMoveTo(rotOfMarker[0], rotOfMarker[1], rotOfMarker[2], rot, 2, 3);
-
-                if(this.last_state_index !== i) {
-                    this.last_state_index = i;
-                    this.updateChildWithMarkers(markers_ids[i]);
-                }
                 break;
             }
         }
 
         el.setAttribute('position', pos);
         el.setAttribute('rotation', rot);
-    },
-    updateChildWithMarkers :function(marker_id) {
-        console.log(`Pushing updated Marker: ${marker_id}`);
-        // this.el.querySelector('*').forEach((e) => e.setAttribute('current_marker', marker_id));
     }
 });
 
